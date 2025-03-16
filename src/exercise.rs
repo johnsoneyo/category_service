f74a96c89c86f4ad8022839d4841035ec68e28d4
= <=>
2025-03-16 21:12:50 +0100

    more exercises added

--- src/exercise.rs
+++ src/exercise.rs
@@ -1,7 +1,9 @@
+use crate::data::Department;
 use std::collections::HashMap;
+use std::io;
+use std::str::FromStr;
 
-
-fn get_median(v: &mut Vec<i32>) -> Option<i32> {
+pub fn get_median(v: &mut Vec<i32>) -> Option<i32> {
     match v.capacity() < 3 {
         true => None,
         false => {
@@ -27,7 +29,7 @@ fn get_median(v: &mut Vec<i32>) -> Option<i32> {
     }
 }
 
-pub(crate) fn get_mode(v: &mut Vec<i32>) -> Option<i32> {
+pub fn get_mode(v: &mut Vec<i32>) -> Option<i32> {
     let mut map = HashMap::new();
 
     for num in v {
@@ -43,4 +45,51 @@ pub(crate) fn get_mode(v: &mut Vec<i32>) -> Option<i32> {
         .unwrap();
 
     Some(mode)
+}
+
+pub fn add_employee() {
+    println!("Welcome to add employee service");
+
+    let mut department_employees: HashMap<Department, Vec<String>> = HashMap::new();
+    let mut department_employees_counter: HashMap<Department, u32> = HashMap::new();
+    loop {
+        println!("please enter the employee name or Help for instructions.");
+        let mut question = String::new();
+
+        match io::stdin().read_line(&mut question) {
+            Ok(n) => {
+
+                match question.trim() {
+                    "Usage" =>  {
+                        println!("Usage: enter ctrl c to stop");
+                        println!("Usage: print to show map");
+                    }
+                    "Print" => {
+                        println!("map elements {department_employees_counter:?}")
+                    }
+                    _ => {
+                        let trimmed_employee = question.trim_ascii();
+                        println!("Please kindly enter the department of employee: {trimmed_employee}");
+                        let mut department = String::new();
+                        io::stdin()
+                            .read_line(&mut department)
+                            .expect("failed to read department");
+
+                        let dept = Department::from_str(&department.trim_ascii())
+                            .expect("couldn't parse department");
+
+                         department_employees.entry(dept)
+                             .or_insert_with(Vec::new)
+                             .push(trimmed_employee.to_string());
+
+                        department_employees_counter.entry(dept)
+                            .and_modify(|e| { *e += 1 })
+                            .or_insert(1);
+
+                    }
+                }
+            }
+            Err(error) => println!("error: {error}"),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 mod exercise;
 mod lesson;
+mod data;
 
 use std::collections::HashMap;
-
+use crate::exercise::add_employee;
+//
+// fn main() {
+//     let mut v = vec![8, 6, 8, 3, 5, 2, 5, 2, 2];
+//
+//     // let median = get_median(&mut v);
+//     // println!("median is {}", median.unwrap_or_default());
+//
+//     let mode = exercise::get_mode(&mut v);
+//     println!("mode is {}", mode.unwrap_or_default());
+// }
 
 fn main() {
-    let mut v = vec![8, 6, 8, 3, 5, 2, 5, 2, 2];
 
-    // let median = get_median(&mut v);
-    // println!("median is {}", median.unwrap_or_default());
-
-    let mode = exercise::get_mode(&mut v);
-    println!("mode is {}", mode.unwrap_or_default());
+    add_employee()
 }
