7addc8615c461f80d31ad088ab49ac414d8a7fff
= <=>
2025-03-16 21:55:19 +0100

    emojis added

--- src/exercise.rs
+++ src/exercise.rs
@@ -2,6 +2,7 @@ use crate::data::Department;
 use std::collections::HashMap;
 use std::io;
 use std::str::FromStr;
+use emojic::flat::{DEPARTMENT_STORE, GREEN_APPLE, GREEN_BOOK, INFORMATION, OFFICE_WORKER, PERSON_FEEDING_BABY, POLICE_OFFICER, STOP_SIGN, THUMBS_UP, WARNING, WAVING_HAND};
 
 pub fn get_median(v: &mut Vec<i32>) -> Option<i32> {
     match v.capacity() < 3 {
@@ -48,12 +49,12 @@ pub fn get_mode(v: &mut Vec<i32>) -> Option<i32> {
 }
 
 pub fn add_employee() {
-    println!("Welcome to add employee service");
+    println!("{} Welcome to add employee service", WAVING_HAND);
 
     let mut department_employees: HashMap<Department, Vec<String>> = HashMap::new();
     let mut department_employees_counter: HashMap<Department, u32> = HashMap::new();
     loop {
-        println!("please enter the employee name or Help for instructions.");
+        println!("{} Please enter the employee name or Usage for instructions.", POLICE_OFFICER);
         let mut question = String::new();
 
         match io::stdin().read_line(&mut question) {
@@ -61,19 +62,19 @@ pub fn add_employee() {
 
                 match question.trim() {
                     "Usage" =>  {
-                        println!("Usage: enter ctrl c to stop");
-                        println!("Usage: print to show map");
+                        println!("{} Usage: enter CTRL-C to stop {}", INFORMATION, STOP_SIGN);
+                        println!("{} Usage: Print to show map", INFORMATION);
                     }
                     "Print" => {
-                        println!("map elements {department_employees_counter:?}")
+                        println!("{} Departments to employees {department_employees:?}", THUMBS_UP)
                     }
                     _ => {
                         let trimmed_employee = question.trim_ascii();
-                        println!("Please kindly enter the department of employee: {trimmed_employee}");
+                        println!("{} Please kindly enter the department of employee: {trimmed_employee}", DEPARTMENT_STORE);
                         let mut department = String::new();
                         io::stdin()
                             .read_line(&mut department)
-                            .expect("failed to read department");
+                            .expect("Failed to read department");
 
                         let dept = Department::from_str(&department.trim_ascii())
                             .expect("couldn't parse department");
@@ -82,9 +83,9 @@ pub fn add_employee() {
                              .or_insert_with(Vec::new)
                              .push(trimmed_employee.to_string());
 
-                        department_employees_counter.entry(dept)
-                            .and_modify(|e| { *e += 1 })
-                            .or_insert(1);
+                        // department_employees_counter.entry(dept)
+                        //     .and_modify(|e| { *e += 1 })
+                        //     .or_insert(1);
 
                     }
                 }
