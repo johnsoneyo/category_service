c354a69649c131a4c3483306b65d67298f555de3
= <=>
2025-03-17 15:15:29 +0100

    data package added

---
+++ src/data/mod.rs
@@ -0,0 +1,25 @@
+use std::str::FromStr;
+
+struct Employee {
+    name: String,
+    department: Department,
+}
+
+#[derive(Eq, Hash, PartialEq, Debug)]
+pub enum Department {
+   Sales,
+    Engineering,
+}
+
+impl FromStr for Department {
+
+     type Err = ();
+
+      fn from_str(input: &str) -> Result<Department, Self::Err> {
+         match input {
+             "eng"  => Ok(Department::Engineering),
+             "sal"  => Ok(Department::Sales),
+             _      => Err(())
+         }
+     }
+ }
